bc4f3d583a4ffb4d51fde40f76d8e94aa96f609f
Dinesh Chukkala <dineshchukkala169@gmail.com>
2022-08-25 17:44:44 +0530

    made all 3 variations

---
+++ public/drag_drop.js
@@ -0,0 +1,162 @@
+"use strict";
+const shopSection = document.querySelector('.shop');
+const cartToggleButton = document.querySelector('.cart-toggle');
+const cartDiv = document.querySelector('.cart');
+class Cart {
+    constructor(cartDiv, cartButton) {
+        this.createToggleFunctionality = () => {
+            this.domButtonElement.addEventListener('click', () => {
+                this.openState = !this.openState;
+                if (this.openState) {
+                    this.domElement.classList.remove('cart-close');
+                    this.domElement.classList.add('cart-open');
+                    this.domButtonElement.textContent = '>>';
+                }
+                else {
+                    this.domElement.classList.remove('cart-open');
+                    this.domElement.classList.add('cart-close');
+                    this.domButtonElement.textContent = '<<';
+                }
+            });
+        };
+        this.initializeDragDropFunctionality = (shopList) => {
+            this.domElement.addEventListener('dragover', (e) => {
+                e.preventDefault();
+            });
+            this.domElement.addEventListener('drop', (e) => {
+                e.preventDefault();
+                try {
+                    const id = parseInt(e.dataTransfer.getData('id'));
+                    this.addToCart(shopList[id - 1]);
+                }
+                catch (err) {
+                    console.error(err);
+                }
+            });
+        };
+        this.createCartElement = (shopObject) => {
+            const divElement = document.createElement('div');
+            divElement.classList.add('cart-object');
+            divElement.id = `cart-object-${shopObject.id}`;
+            const divImgElement = document.createElement('div');
+            divImgElement.classList.add('cart-object-img');
+            const imgElement = document.createElement('img');
+            imgElement.src = shopObject.image;
+            imgElement.draggable = false;
+            divImgElement.appendChild(imgElement);
+            const divDescElement = document.createElement('div');
+            divDescElement.classList.add('cart-object-desc');
+            const spanElement = document.createElement('span');
+            spanElement.innerText = shopObject.name;
+            const paragraphElement = document.createElement('p');
+            paragraphElement.innerText = shopObject.desc;
+            const qtyDivElement = document.createElement('div');
+            qtyDivElement.classList.add('cart-object-qty');
+            const spanQuantityElement = document.createElement('span');
+            spanQuantityElement.classList.add('quantity');
+            spanQuantityElement.innerText = 'Qty: x1';
+            const incQtyButton = document.createElement('button');
+            incQtyButton.textContent = '+';
+            incQtyButton.onclick = () => {
+                console.log('click');
+                this.checkAndChangeQuantity(shopObject, 1);
+            };
+            const descQtyButton = document.createElement('button');
+            descQtyButton.textContent = '-';
+            descQtyButton.onclick = () => {
+                this.checkAndChangeQuantity(shopObject, -1);
+            };
+            qtyDivElement.appendChild(incQtyButton);
+            qtyDivElement.appendChild(spanQuantityElement);
+            qtyDivElement.appendChild(descQtyButton);
+            divDescElement.appendChild(spanElement);
+            divDescElement.appendChild(paragraphElement);
+            divDescElement.appendChild(qtyDivElement);
+            divElement.appendChild(divImgElement);
+            divElement.appendChild(divDescElement);
+            return divElement;
+        };
+        this.modifyCartContent = (foundCartObject) => {
+            if (foundCartObject.quantity === 0) {
+                const element = document.querySelector(`#cart-object-${foundCartObject.id}`);
+                this.listContainerElement.removeChild(element);
+                this.cartContents = this.cartContents.filter((cartObject) => cartObject.id !== foundCartObject.id);
+                return;
+            }
+            const contentElement = document.querySelector(`#cart-object-${foundCartObject.id} > .cart-object-desc .quantity`);
+            contentElement.innerText = `Qty: x${foundCartObject.quantity}`;
+        };
+        this.checkAndChangeQuantity = (shopObject, delta) => {
+            const foundCartObject = this.cartContents.find((cartObject) => cartObject.id === shopObject.id);
+            if (foundCartObject !== undefined) {
+                foundCartObject.quantity += delta;
+                this.modifyCartContent(foundCartObject);
+                return true;
+            }
+            return false;
+        };
+        this.addToCart = (shopObject) => {
+            if (this.checkAndChangeQuantity(shopObject, 1))
+                return;
+            this.cartContents.push({ ...shopObject, quantity: 1 });
+            this.listContainerElement.appendChild(this.createCartElement(shopObject));
+            if (!this.openState)
+                this.domButtonElement.click();
+        };
+        this.domElement = cartDiv;
+        this.openState = false;
+        this.domButtonElement = cartButton;
+        this.listContainerElement = document.querySelector('.cart-object-container');
+        this.cartContents = [];
+        this.createToggleFunctionality();
+    }
+}
+const getData = async () => {
+    const response = await fetch('./data.json', {
+        method: 'GET',
+    });
+    console.log(response);
+    return await response.json();
+};
+const appendData = (shopList) => {
+    const shopChildren = shopList.map((shopObject) => {
+        const objectElement = document.createElement('div');
+        objectElement.classList.add('card');
+        objectElement.id = `card-id-${shopObject.id}`;
+        objectElement.draggable = true;
+        const divImageElement = document.createElement('div');
+        divImageElement.classList.add('card-img');
+        const imageElement = document.createElement('img');
+        imageElement.src = shopObject.image;
+        imageElement.draggable = false;
+        const divDescElement = document.createElement('div');
+        divDescElement.classList.add('card-desc');
+        const spanElement = document.createElement('span');
+        spanElement.innerText = shopObject.name;
+        const paragraphElement = document.createElement('p');
+        paragraphElement.innerText = shopObject.desc;
+        divDescElement.appendChild(spanElement);
+        divDescElement.appendChild(paragraphElement);
+        divImageElement.appendChild(imageElement);
+        objectElement.appendChild(divImageElement);
+        objectElement.appendChild(divDescElement);
+        return objectElement;
+    });
+    shopChildren.forEach((shopElement) => {
+        shopSection?.appendChild(shopElement);
+    });
+    return shopChildren;
+};
+const main = async () => {
+    const data = await getData();
+    const shopChildren = appendData(data.list);
+    const cart = new Cart(cartDiv, cartToggleButton);
+    shopChildren.forEach((shopElement, idx) => {
+        shopElement.addEventListener('dragstart', (e) => {
+            // e.preventDefault();
+            e.dataTransfer?.setData('id', `${data.list[idx].id}`);
+        });
+    });
+    cart.initializeDragDropFunctionality(data.list);
+};
+main();
