368f9abc1dbfa990d7559e5f3fdc48f04dc1c2f1
Dinesh Chukkala <dineshchukkala169@gmail.com>
2023-06-24 21:03:39 +0530

    analytics

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const path = require('path');
 const fs = require('fs');
 const app = express();
 
+const { authorize } = require('./google-api')
+const { google } = require('googleapis');
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
@@ -25,6 +28,43 @@ app.get('/select', (_, res) => {
         return res.send('random');
     }
 });
+
+const appendToSheet = async (values) => {
+    values = [values]
+    const resource = { values }
+    const spreadsheetId = '1tpcDD7k9RzkiWGYeyTJ00oV58Fpcqn0c-vwiRed2gwE'
+    const sheetName = 'Sheet1'
+
+    try {
+        const auth = await authorize()
+        const service = google.sheets({ version: 'v4', auth })
+
+        const result = await service.spreadsheets.values.append({
+            spreadsheetId,
+            range: `${sheetName}!2:3`,
+            insertDataOption: 'INSERT_ROWS',
+            valueInputOption: 'RAW',
+            resource
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+app.post('/submit-details', async (req, res) => {
+    let { body: { cost, quantity, startTime, endTime }} = req
+    const ipAddress = req.socket.remoteAddress
+
+    startTime = new Date(startTime)
+    endTime = new Date(endTime)
+
+    try {
+        await appendToSheet([ipAddress, startTime.toString(), endTime.toString(), quantity, cost])
+        res.status(200).json({ success: true })
+    } catch (_) {
+        res.status(500).json({ success: false })
+    }
+})
 
 app.post('/admin', (req, res) => {
     const select = req.body.select;
